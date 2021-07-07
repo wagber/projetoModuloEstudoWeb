515b067fc740aab02c7d633b8a04d8abe3e24c89
Wagner Bernardo <wagber.dev@gmail.com>
2021-07-07 17:20:38 -0300

    Ajustado arquivo index.php

--- index.php
+++ index.php
@@ -18,8 +18,6 @@
                     <h3>Módulo 01</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
                     </ul>
                 </div>
             </nav>
