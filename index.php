fcff2ff3ce704a2ca74986955c494f03dee63353
Wagner Bernardo <wagber.dev@gmail.com>
2021-07-09 22:24:07 -0300

    Adicionado dados no arquivo index.php

--- index.php
+++ index.php
@@ -15,9 +15,21 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01</h3>
+                    <h3>Básico</h3>
                     <ul>
-                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=desafio">Desafio PHP</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>Tipos</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=tipos&file=int">Tipo Inteiro</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=float">Tipo Float</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=aritmeticas">Op. Aritméticas</a></li>
                     </ul>
                 </div>
             </nav>
